038357ba0dca04f2b83b6f2cff32a16a9741dfd6
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-03-31 14:37:17 -0500

    Agrega mensajes de validación personalizados y mejora el manejo de errores en AuthController.php y app.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,12 +12,25 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $messages = [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.max' => 'El nombre no puede tener más de 255 caracteres.',
+            'email.required' => 'El campo correo electronico es obligatorio.',
+            'email.email' => 'El correo electronico debe ser una dirección de correo electrónico válida.',
+            'email.unique' => 'El correo electronico ya esta en uso.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.min' => 'La contraseña debe tener al menos 6 caracteres.',
+            'role.required' => 'El campo rol es obligatorio.',
+            'role.in' => 'El rol debe ser admin o user.',
+            'role.exists' => 'El rol debe ser admin o user.',
+        ];
+
         $validatedData = $request->validate([
             'name' => 'required|max:255',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
             'role' => 'required|in:admin,user',
-        ]);
+        ], $messages);
 
         $validatedData['password'] = Hash::make($request->password);
 
@@ -25,18 +38,18 @@ class AuthController extends Controller
 
         $token = JWTAuth::fromUser($user);
 
-        return response([ 'user' => $user, 'token' => $token], 201);
+        return response(['user' => $user, 'token' => $token], 201);
     }
 
     public function login(Request $request)
     {
         $credentials = $request->validate([
             'email' => 'required|email',
-            'password' => 'required|min:6',
+            'password' => 'required',
         ]);
     
-        if (! $token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Las credenciales proporcionadas son incorrectas.'], 401);
         }
     
         return response()->json([
@@ -44,4 +57,5 @@ class AuthController extends Controller
             'token' => $token,
         ]);
     }
+    
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(function (ValidationException $e, $request) {
-            return response()->json(['error' => 'Los datos proporcionados no son validos.'], 422);
+            return response()->json(['errors' => $e->errors()], $e->status);
         });
     })
     ->create();
