1963161c634cb9e1e2099cbc6757a38d293f7d27
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-03-31 11:49:10 -0500

    Agrega campo de rol a la tabla de usuarios y reorganiza rutas en api.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role'); // Agrega esta línea
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::post('/registro', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReportController;
 
-Route::post('/generate-report', [ReportController::class, 'generate']);
+// Rutas que no requieren autenticación
+Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::group(['middleware' => 'auth:api'], function() {
+Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    // Mover todas las rutas que requieren autenticación aquí
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::post('/generate-report', [ReportController::class, 'generate']);
 });
